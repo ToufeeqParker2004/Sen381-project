65e333e38d809ef6ea6bf5a8645412a7a8a61826
Renaldo Jardim <21063944+Renaldo9@users.noreply.github.com>
2025-10-15 18:13:57 +0200

    Created basic notification system
    Still needs:
    - dedicated notification page
    - Triggers on tables to add into notifications automatically
    - Adjustments to UI design

--- Backend/src/main/java/com/backend/Java_Backend/Repository/NotificationRepository.java
+++ Backend/src/main/java/com/backend/Java_Backend/Repository/NotificationRepository.java
@@ -2,9 +2,17 @@ package com.backend.Java_Backend.Repository;
 
 import com.backend.Java_Backend.Models.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    @Query("SELECT n FROM Notification n WHERE n.user_id = :userId ORDER BY n.created_at DESC")
+    List<Notification> findByUserId(@Param("userId") Integer userId);
+
+    @Query("SELECT n FROM Notification n WHERE n.user_id = :userId AND n.status = 'unread' ORDER BY n.created_at DESC")
+    List<Notification> findUnreadByUserId(@Param("userId") Integer userId);
 }
--- Backend/src/main/java/com/backend/Java_Backend/Repository/SubscribeNotiRepository.java
+++ Backend/src/main/java/com/backend/Java_Backend/Repository/SubscribeNotiRepository.java
@@ -4,10 +4,12 @@ import com.backend.Java_Backend.Models.NotificationSubscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SubscribeNotiRepository extends JpaRepository<NotificationSubscription, UUID> {
     Optional<NotificationSubscription> findByStudentID(Integer studentID);
+    List<NotificationSubscription> findBySubscribedTrue();
 }
